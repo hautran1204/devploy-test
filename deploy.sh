de665e07aa6c283c26bd7c70467db95e148fa1dc
hau tran <hau.tht@neo-lab.vn>
2020-12-30 17:23:03 +0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 # abort on errors
 set -e
-# buildyarn run build
-# navigate into the build output directorycd dist
+# build
+yarn run build
+# navigate into the build output directory
+cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 git init
